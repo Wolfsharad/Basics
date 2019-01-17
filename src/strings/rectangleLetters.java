39110f543891fbec89192e78ab2c4560ff0f0a40
10614974 <10614974@172.28.101.47>
2019-01-17 21:33:19 +0530

    AAA BBB CCC AlphabetsRectangle

---
+++ src/strings/rectangleLetters.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class rectangleLetters {
+	public static void main(String[] args) {
+		int num = 5;
+		for (int i = 0; i < num; i++) {
+			System.out.println();
+			for (int j = 0; j < num; j++) {
+				System.out.print((char) (65 + i));
+			}
+		}
+	}
+}
